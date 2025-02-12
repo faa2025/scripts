968ffe7d988224f97153e0ca2017b8d1f589a390
hxrda <143107271+hxrda@users.noreply.github.com>
2025-02-12 09:20:55 +0200

    Create echo_camera.sh

---
+++ scripts/schedulers/monitor-daylight/echo_camera.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#This script is triggered by daylight_monitor.sh
+
+while true; do
+    echo "It is daylight"
+    sleep 5  # Print every 5 seconds
+    #sleep 3600  # Print once every hour
+done
