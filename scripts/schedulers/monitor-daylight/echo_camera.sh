6a292bf8f2902097c0985db4c57550e031de6f32
hxrda <143107271+hxrda@users.noreply.github.com>
2025-02-20 19:04:51 +0200

    Delete scripts/schedulers/monitor-daylight/echo_camera.sh

--- scripts/schedulers/monitor-daylight/echo_camera.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#This script is triggered by daylight_monitor.sh
-
-while true; do
-    echo "It is daylight"
-    sleep 5  # Print every 5 seconds
-    #sleep 3600  # Print once every hour
-done
